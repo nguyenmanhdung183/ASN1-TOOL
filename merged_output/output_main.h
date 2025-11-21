100e2446a1d3626d9a0053e694291fc98f644196
Nguyen Manh Dung <Dung.NM213838@sis.hust.edu.vn>
2025-11-21 13:52:29 +0700

    a

--- merged_output/output_main.h
+++ merged_output/output_main.h
@@ -309,11 +309,15 @@ EXTERN int asn1PrtToStrm_e2apE2connectionUpdate_protocolIEs (OSCTXT* pctxt, cons
 
 
 typedef enum{
-   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_IEs_UNDEF_,
-   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_IEs_id_TransactionID ,
-   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_IEs_id_E2connectionUpdate_List ,
-   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_IEs_id_E2connectionUpdateRemove_List ,
-   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_IEs_id_E2connectionUpdate_List 
+   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_UNDEF_,
+   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_id_TransactionID,
+
+   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_id_E2connectionUpdate_List,
+
+   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_id_E2connectionUpdateRemove_List,
+
+   T_E2AP_PDU_Contents_e2ap_E2connectionUpdate_id_E2connectionUpdate_List,
+
    T_E2AP_PDU_Contents_e2ap_E2connectionUpdateIEs_id_Extended_RANNodeName_
 
 }e2ap_E2connectionUpdate_IEs_TVALUE;
